d5ed387c6e3d1a5fda067c0c3590386ec08ed231
Eugene015 <egavriluk@gmail.com>
2022-04-18 19:35:45 +0200

    first commit

---
+++ src/Users/AddUser.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AddUser = (props) => {
+    <form>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' />
+    </form>
+};
+
+export default AddUser; 
